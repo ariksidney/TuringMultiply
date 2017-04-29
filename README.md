7b6d42f7dd6a73911d455454650fdcf93b39bc30
Arik Sidney Guggenheim <arik.guggenheim@arik.sydney>
2017-04-29 16:04:48 +0200

    added normal number entering mode

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Turing machine to multiply unary numbers
 
 ## Usage
 The numbers to multiply must be given as arguments like this: *** **** (for 3 * 4)
+
+You can also enter normal numbers like 3 4 (for 3 * 4)
+
--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,14 @@ public class Main {
             if(args[i].startsWith("*")) {
                 input.add(args[i]);
             }
+            else if (args[i].matches("^-?\\d+$")) {
+                StringBuilder builder = new StringBuilder();
+                int numbr = Integer.parseInt(args[i]);
+                for(int j = 0; j < numbr; j++) {
+                    builder.append("*");
+                }
+                input.add(builder.toString());
+            }
         }
         Band band = new Band(input.get(0)+ " " + input.get(1));
         TM tm = new TM(band);
